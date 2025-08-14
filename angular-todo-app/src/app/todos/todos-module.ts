71edaab30af894165d38c60e4a67ac4ba682d838
kemo audrey <kemoaudrey17gmail.com>
2025-08-14 18:02:55 +0100

    feat(todos): add todo components and required modules
    - Add TodoListComponent and TodoModalComponent to declarations
    - Import necessary Angular Material modules for UI components
    - Include ReactiveFormsModule and Ng2SmartTableModule for form handling and data display

--- angular-todo-app/src/app/todos/todos-module.ts
+++ angular-todo-app/src/app/todos/todos-module.ts
@@ -1,14 +1,53 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
+// Angular Material
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { TodosRoutingModule } from './todos-routing-module';
-
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoModalComponent } from './todo-modal/todo-modal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TodoListComponent,
+    TodoModalComponent
+  ],
   imports: [
     CommonModule,
-    TodosRoutingModule
+    TodosRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    Ng2SmartTableModule,
+
+    // Angular Material
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatTooltipModule
   ]
 })
 export class TodosModule { }
