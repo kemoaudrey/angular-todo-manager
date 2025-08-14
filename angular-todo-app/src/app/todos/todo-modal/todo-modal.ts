0c63daf5266e6896b87fb566b0c53ee0d67c5463
kemo audrey <kemoaudrey17gmail.com>
2025-08-14 14:44:26 +0100

    feat: add todo-modal and person-list components
    Add new Angular components for todo modal and person list features. Each component includes HTML template, CSS styles, TypeScript class, and corresponding test files.

---
+++ angular-todo-app/src/app/todos/todo-modal/todo-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-modal',
+  imports: [],
+  templateUrl: './todo-modal.html',
+  styleUrl: './todo-modal.css'
+})
+export class TodoModal {
+
+}
