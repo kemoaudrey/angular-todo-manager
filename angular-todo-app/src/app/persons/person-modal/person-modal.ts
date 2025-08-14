83ab14014e403ad32050851d98208552920d29e1
kemo audrey <kemoaudrey17gmail.com>
2025-08-14 14:45:56 +0100

    feat(persons): add person modal component
    Add new Angular component for person modal dialog including:
    - Basic component class
    - Template placeholder
    - Empty CSS file
    - Unit test setup

---
+++ angular-todo-app/src/app/persons/person-modal/person-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-modal',
+  imports: [],
+  templateUrl: './person-modal.html',
+  styleUrl: './person-modal.css'
+})
+export class PersonModal {
+
+}
