24a0ca563c2e7df4d441f6b3000a5b67c15110e0
kemo audrey <kemoaudrey17gmail.com>
2025-08-14 18:04:21 +0100

    feat(persons): add person list and modal components with Angular Material
    Add PersonListComponent and PersonModalComponent to persons module along with required Angular Material modules and form modules for functionality. This enables CRUD operations for persons in the application.

--- angular-todo-app/src/app/persons/persons-module.ts
+++ angular-todo-app/src/app/persons/persons-module.ts
@@ -1,14 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// Angular Material
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { PersonsRoutingModule } from './persons-routing-module';
-
+import { PersonListComponent } from './person-list/person-list.component';
+import { PersonModalComponent } from './person-modal/person-modal.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PersonListComponent,
+    PersonModalComponent
+  ],
   imports: [
     CommonModule,
-    PersonsRoutingModule
+    PersonsRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+
+    // Angular Material
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ]
 })
 export class PersonsModule { }
