eff15fb4d3cbadadb1ee3f0461523f2044872b65
kemo audrey <kemoaudrey17gmail.com>
2025-08-14 14:59:24 +0100

    feat(models): add Priority and Label enums for todo items
    These enums will be used to categorize and prioritize todo items in the application. Priority enum defines difficulty levels while Label enum categorizes items by technology.

---
+++ angular-todo-app/src/app/models/enums.ts
@@ -0,0 +1,12 @@
+export enum Priority {
+  FACILE = 'Facile',
+  MOYEN = 'Moyen',
+  DIFFICILE = 'Difficile'
+}
+
+export enum Label {
+  HTML = 'HTML',
+  CSS = 'CSS',
+  NODE_JS = 'NODE JS',
+  JQUERY = 'JQUERY'
+}
