6b8f262ad0db8a2db6e8c1204f2c1e08aeac4234
kemo audrey <kemoaudrey17gmail.com>
2025-08-14 18:01:29 +0100

    feat(persons): add routing for person list component
    Add route configuration to display PersonListComponent as default view for persons module

--- angular-todo-app/src/app/persons/persons-routing-module.ts
+++ angular-todo-app/src/app/persons/persons-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonListComponent } from './person-list/person-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PersonListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
